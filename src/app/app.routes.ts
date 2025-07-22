f927ee1511fe6560d0f3dfed877c390b3cb759ff
NAVEEN171 <naveenkumar171837@gmail.com>
2025-07-22 17:34:54 +0530

    debouncing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { UsersList } from './users-list/users-list';
+import { SearchWeather } from './search-weather/search-weather';
 
 export const routes: Routes = [
   {
     path: '',
     component: UsersList,
+  },
+  {
+    path: 'weather',
+    component: SearchWeather,
   },
 ];
